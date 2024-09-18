7cbe47a5c05d0db454c7590d597ca4ac65aa5b39
Vanshika Sharma <124205763+vanshikaashrma@users.noreply.github.com>
2024-09-18 21:25:20 +0530

    Update stackusingll.cpp
    fixed errors

--- stackusingll.cpp
+++ stackusingll.cpp
@@ -1,83 +1,92 @@
 #include <iostream>
-
-class StackNode {
+using namespace std;
+class Node
+{
 public:
-    int data;
-    StackNode* next;
-
-    StackNode(int value) : data(value), next(nullptr) {}
+int data;
+Node *next;
+Node(int data)
+{
+  this->data=data;
+  this->next=NULL;
+}
 };
-
-class Stack {
-private:
-    StackNode* top;
-    int size;
-
+class Stack
+{
 public:
-    Stack() : top(nullptr), size(0) {}
-
-    // Push an element onto the stack
-    void push(int value) {
-        StackNode* newNode = new StackNode(value);
-        newNode->next = top;
-        top = newNode;
-        size++;
+Node* top;
+int size;
+Stack()
+{
+  top=NULL;
+  size=0;
+}
+~Stack()
+{
+  
+  while(top!=NULL)
+    { Node* temp=top;
+      top=top->next;
+      delete temp;
+      
     }
+}
+void push(int value)
+{
+  Node* temp= new Node(value);
+  temp->next=top;
+  top=temp;
+  size++;
+}
 
-    // Pop an element from the stack
-    int pop() {
-        if (isEmpty()) {
-            std::cerr << "Stack underflow!" << std::endl;
-            return -1; // or handle the error as needed
-        }
-        int topData = top->data;
-        StackNode* temp = top;
-        top = top->next;
-        delete temp;
-        size--;
-        return topData;
+int pop()
+{
+  if(top==NULL)
+    cout<<"stack underflow"<<endl;
+  int d= top->data;
+  Node* temp= top;
+  top=top->next;
+  delete temp;
+  size--;
+  return d;
+}
+void print()
+{
+  Node* temp=top;
+  while(temp!=NULL)
+    {
+      cout<<temp->data<<" ";
+      temp=temp->next;
     }
-
-    // Check if the stack is empty
-    bool isEmpty() const {
-        return top == nullptr;
-    }
-
-    // Get the current size of the stack
-    int getSize() const {
-        return size;
-    }
-
-    // Peek at the top element without removing it
-    int peek() const {
-        if (isEmpty()) {
-            std::cerr << "Stack is empty!" << std::endl;
-            return -1; // or handle the error as needed
-        }
-        return top->data;
-    }
-
-    // Destructor to free memory
-    ~Stack() {
-        while (!isEmpty()) {
-            pop();
-        }
-    }
+  cout<<endl;
+}
+int peek()
+{
+  if(top==NULL)
+    cout<<"stack underflow"<<endl;
+  return top->data;
+}
+bool isEmpty()
+{
+  if(size==0)
+    return true;
+  else
+    return false;
+}
+int sizeOfStack()
+{
+  return size;
+}
 };
-
-int main() {
-    Stack stack;
-
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-
-    std::cout << "Top element is: " << stack.peek() << std::endl; // Should output 30
-
-    std::cout << "Popped element: " << stack.pop() << std::endl; // Should output 30
-    std::cout << "Popped element: " << stack.pop() << std::endl; // Should output 20
-
-    std::cout << "Current size of stack: " << stack.getSize() << std::endl; // Should output 1
-
-    return 0;
+int main()
+{
+  Stack s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.print();
+  s.pop();
+  cout<<(s.isEmpty()?"true":"false")<<endl;
+  cout<<s.peek()<<endl;
+  cout<<s.sizeOfStack()<<endl;
 }
